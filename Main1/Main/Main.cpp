825f1765e7a80c369e9508ece795e00c0b20aa35
Miroslav Chobanov <MSChobanov21@codingburgas.bg>
2024-04-20 20:35:46 +0300

    Add Main menu display function

---
+++ Main1/Main/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+void displayMainMenu()
+{
+    cout << endl << "------ Digital School Main Menu ------" << endl;
+    cout << "1. viewCourses" << endl;
+    cout << "2. completedCourses" << endl;
+    cout << "3. Grades" << endl;
+}
