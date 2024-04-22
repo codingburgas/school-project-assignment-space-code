9aef824f06e7c7a32351b2d38e7e701ae71346fa
LKDimov21 <LKDimov21@codingburgas.bg>
2024-04-22 21:27:18 +0300

    Add physics header

---
+++ Main1/Main/physics.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <iostream>
+using namespace std;
+void physics(int points, char answer[]);
+void backToMainPhy();
+void GradesPhy();
--- Main1/Main/viewCourses.cpp
+++ Main1/Main/viewCourses.cpp
@@ -9,6 +9,7 @@ void viewCourses(int points, char& option, char answer[]) // Use references
     cout << "1. Programming" << endl;
     cout << "2. Maths" << endl;
     cout << "3. Biology" << endl;
+    cout << "4. Physics" << endl;
     cin >> option;
     system("cls");
     switch (option)
@@ -16,6 +17,7 @@ void viewCourses(int points, char& option, char answer[]) // Use references
     case '1': programming(points, answer); break;
     case '2': maths(points, answer); break;
     case '3': biology(points, answer); break;
+    case '4': physics(points, answer); break;
     default: cout << "Invalid choice. Please enter a valid option.";
     }
 }
